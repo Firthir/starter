171f4c13e876655d985ff913be9282f77205b2b6
Frithir <alex@frithir.com>
2021-05-24 12:48:17 +1000

    tailwind4

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import '@/tailwind.css';
-import 'splitting/dist/splitting.css';
-import 'splitting/dist/splitting-cells.css';
+import './assets/tailwind.css';
+// import 'splitting/dist/splitting.css';
+// import 'splitting/dist/splitting-cells.css';
 
 Vue.config.productionTip = false;
 
